6fb774702d90d5e1113929be17415064399f1525
zrnx <nolanr6969@gmail.com>
2020-01-24 07:33:40 +0000

    Added card class with suits, faces and values. Added deck as a list of cards with shuffle function

--- BlackjackLogic/Card.cs
+++ BlackjackLogic/Card.cs
@@ -6,7 +6,91 @@ using System.Threading.Tasks;
 
 namespace BlackjackLogic
 {
-    class Card
+    public enum Suit
     {
+        Heart,
+        Diamond,
+        Spade,
+        Club
+    }
+
+    public enum Face
+    {
+        Ace,
+        Two,
+        Three,
+        Four,
+        Five,
+        Six,
+        Seven,
+        Eight,
+        Nine,
+        Ten,
+        Jack,
+        Queen,
+        King,
+    }
+
+
+    public class Card
+    {
+        public Suit Suit { get; set; }
+        public Face Face { get; set; }
+        public int Value { get; set; }
+
+        public Card(Suit suit, Face face)
+        {
+            Suit = suit;
+            Face = face;
+            switch (face)
+            {
+                case Face.Ace:
+                    Value = 1; // 1 or 11
+                    break;
+                case Face.Two:
+                    Value = 2;
+                    break;
+                case Face.Three:
+                    Value = 3;
+                    break;
+                case Face.Four:
+                    Value = 4;
+                    break;
+                case Face.Five:
+                    Value = 5;
+                    break;
+                case Face.Six:
+                    Value = 6;
+                    break;
+                case Face.Seven:
+                    Value = 7;
+                    break;
+                case Face.Eight:
+                    Value = 8;
+                    break;
+                case Face.Nine:
+                    Value = 9;
+                    break;
+                case Face.Ten:
+                    Value = 10;
+                    break;
+                case Face.Jack:
+                    Value = 11; // 10 in Blackjack
+                    break;
+                case Face.Queen:
+                    Value = 12; // 10 in Blackjack
+                    break;
+                case Face.King:
+                    Value = 13; // 10 in Blackjack
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{Face} of {Suit}s";
+        }
     }
 }
--- BlackjackLogic/Deck.cs
+++ BlackjackLogic/Deck.cs
@@ -6,7 +6,55 @@ using System.Threading.Tasks;
 
 namespace BlackjackLogic
 {
-    class Deck
+    public class Deck
     {
+        public Stack<Card> Cards = new Stack<Card>();
+
+        public Deck()
+        {
+            BuildDeck();
+        }
+
+        private void Shuffle()
+        {
+            var stackToArray = Cards.ToArray();
+            var rnd = new Random();
+            Cards.Clear();
+            foreach (var card in stackToArray.OrderBy(x => rnd.Next()))
+            {
+                Cards.Push(card);
+            }
+        }
+
+        private bool VerifyDeck(Stack<Card> cards)
+        {
+            if (cards.Count != cards.Distinct().Count())
+            {
+                Console.WriteLine("FALSE");
+                return false;
+            }
+            Console.WriteLine("TRUE");
+            return true;
+        }
+
+        public void BuildDeck()
+        {
+
+            foreach (Suit s in Enum.GetValues(typeof(Suit)))
+            {
+                foreach (Face f in Enum.GetValues(typeof(Face)))
+                {
+                    Cards.Push(new Card(s, f));
+                }
+            }
+            Shuffle();
+
+
+            //foreach (var c in Cards)
+            //{
+            //    Console.WriteLine(c);
+            //}
+            VerifyDeck(Cards);
+        }
     }
 }
