a9b984ade452558043f14553f5612ff6e361ec5a
zrnx <nolanr6969@gmail.com>
2020-01-27 15:34:07 +0000

    Added basic argument parsing function for shuffling frequency and amount of hands played.

--- BlackjackStrategy/Program.cs
+++ BlackjackStrategy/Program.cs
@@ -13,6 +13,29 @@ namespace BlackjackStrategy
         {
             //Parse args
             //init game loop and pass parsed args
+
+            int turnsToBePlayed = 200;
+            int cardsBeforeShuffling = 26;
+
+            for (int i = 0; i<args.Length;i++)
+            {
+                switch (args[i])
+                {
+                    case "--t":
+                    case "-hands":
+                        turnsToBePlayed = int.Parse(args[i + 1]);
+                        break;
+                    case "--s":
+                    case "-shuffle":
+                        cardsBeforeShuffling = int.Parse(args[i + 1]);
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+            //Game _game = new Game(turnsToBePlayed, cardsBeforeShuffling);
+
             Game _game = new Game();
         }
     }
