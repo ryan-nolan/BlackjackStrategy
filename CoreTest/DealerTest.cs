89cb58ff6f71958f272e39cf2b96324c4dda74c6
zrnx <nolanr6969@gmail.com>
2020-07-25 19:47:58 +0100

    Added Unit Test Project for testing the game core. Completed Dealer testing.

---
+++ CoreTest/DealerTest.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BlackjackLogic;
+using BlackjackLogic.Strategies;
+
+namespace CoreTest
+{
+    [TestClass]
+    public class DealerTest
+    {
+        [TestMethod]
+        public void StandOnSoft17Test()
+        {
+
+            PlayerState expectedState = PlayerState.STAND;
+            Dealer dealer = new Dealer
+            {
+                hand = new Hand
+                {
+                    cards = new List<Card>()
+                    {
+                        new Card(Suit.Club, Face.Ace),
+                        new Card(Suit.Club, Face.Six)
+                    }
+                }
+            };
+            PlayerState state = (dealer.React());
+            Assert.AreEqual(state, expectedState);
+        }
+        [TestMethod]
+        public void HitOnLessThanHard17Test()
+        {
+
+            PlayerState expectedState = PlayerState.HIT;
+            Dealer dealer = new Dealer
+            {
+                hand = new Hand
+                {
+                    cards = new List<Card>()
+                    {
+                        new Card(Suit.Heart, Face.Five),
+                        new Card(Suit.Club, Face.Five)
+                    }
+                }
+            };
+            PlayerState state = (dealer.React());
+            Assert.AreEqual(state, expectedState);
+        }
+        [TestMethod]
+        public void StandOnGreaterThanHard17Test()
+        {
+
+            PlayerState expectedState = PlayerState.STAND;
+            Dealer dealer = new Dealer
+            {
+                hand = new Hand
+                {
+                    cards = new List<Card>()
+                    {
+                        new Card(Suit.Heart, Face.Ten),
+                        new Card(Suit.Club, Face.Ten)
+                    }
+                }
+            };
+            PlayerState state = (dealer.React());
+            Assert.AreEqual(state, expectedState);
+        }
+    }
+}
---
+++ CoreTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CoreTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CoreTest")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4141990d-86f9-4a05-97af-83a6fa1bcb09")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
